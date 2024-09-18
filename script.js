dcd6743efc413d2c7357efdc1ba6d4837ab1eade
Steven-LG09 <stevegiraldo224@gmail.com>
2024-09-18 15:59:29 -0400

    Api

---
+++ script.js
@@ -0,0 +1,84 @@
+fetch("https://fake-api-vq1l.onrender.com/posts", {
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjE1LCJlbWFpbCI6InN0ZXZlbi5sb3BlejJAdXRwLmVkdS5jbyIsImlhdCI6MTcyNjY4MTkzMiwiZXhwIjoxNzQzOTYxOTMyfQ.B4COfQ30uuvJjfW-t7nRCm6uSoFq_sVpEip5iy5S6rQ"
+    }
+})
+  //pasarlo a json
+  .then(response => response.json())
+  //procesar la info
+  .then(data => {
+
+    const list = document.getElementById("list");
+
+
+    data.forEach( product => {
+      const il = document.createElement("li");
+      const images = JSON.parse(product.images)
+      const myhtml = ` 
+        <div class="card" style="width: 18rem;">
+          <img src="${images[0]}" class="card-img-top" alt="error en imagen">
+          <div class="card-body">
+            <h5 class="card-title">${product.title}</h5>
+            <p class="card-text">${product.value}</p>
+            <a class="btn btn-warning">Edit</a>
+            <a onclick="deletePost(${product.id})" class="btn btn-danger">Delete</a>
+          </div>
+        </div>
+      `; 
+      il.innerHTML =  myhtml;
+      list.appendChild(il); 
+    })
+  });
+
+
+function sendForm(){
+  const title = document.getElementById("title");
+  const description = document.getElementById("description");
+  const value = document.getElementById("value");
+  const image = document.getElementById("image");
+  const body ={
+    title: title.value,
+    description: description.value,
+    value: value.value,
+    images: [image.value] 
+  }
+
+
+  fetch("https://fake-api-vq1l.onrender.com/posts", {
+    method: "POST", 
+    headers: {
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjE1LCJlbWFpbCI6InN0ZXZlbi5sb3BlejJAdXRwLmVkdS5jbyIsImlhdCI6MTcyNjY4MTkzMiwiZXhwIjoxNzQzOTYxOTMyfQ.B4COfQ30uuvJjfW-t7nRCm6uSoFq_sVpEip5iy5S6rQ",
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(body)
+  })
+  .then( res => res.json())
+  .then( res => {
+    console.log(
+      "respuesta de la api", res
+    )
+    title.value = "";
+    description.value = "";
+    value.value = "";
+    image.value = "";
+    location.reload();
+  })
+
+}
+
+function deletePost(id){
+  fetch(`https://fake-api-vq1l.onrender.com/posts/${id}`, {
+    method: "DELETE", 
+    headers: {
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjE1LCJlbWFpbCI6InN0ZXZlbi5sb3BlejJAdXRwLmVkdS5jbyIsImlhdCI6MTcyNjY4MTkzMiwiZXhwIjoxNzQzOTYxOTMyfQ.B4COfQ30uuvJjfW-t7nRCm6uSoFq_sVpEip5iy5S6rQ",
+    },
+  })
+  .then( res => res.json())
+  .then( res => {
+    console.log(
+      "respuesta de la api", res
+    )
+    location.reload();
+  })
+}
