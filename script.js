025c264f1a58a6beed9ede7c4537206f0b4b1547
Steven-LG09 <stevegiraldo224@gmail.com>
2024-09-21 21:20:27 -0400

    Api3

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ fetch("https://fake-api-vq1l.onrender.com/posts", {
       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjE1LCJlbWFpbCI6InN0ZXZlbi5sb3BlejJAdXRwLmVkdS5jbyIsImlhdCI6MTcyNjY4MTkzMiwiZXhwIjoxNzQzOTYxOTMyfQ.B4COfQ30uuvJjfW-t7nRCm6uSoFq_sVpEip5iy5S6rQ"
     }
 })
-  //pasarlo a json
   .then(response => response.json())
-  //procesar la info
   .then(data => {
 
     const list = document.getElementById("list");
@@ -16,15 +14,25 @@ fetch("https://fake-api-vq1l.onrender.com/posts", {
       const il = document.createElement("li");
       const images = JSON.parse(product.images)
       const myhtml = ` 
-        <div class="card " style="width: 18rem; border: 2px solid #F5E7B2;background-color: #F5E7B2;">
+        <div class="card " style="width: 18rem; border: 2px solid #F5E7B2;background-color: #F5E7B2;
+        box-shadow: 0 12px 16px 0 #d1ab7c, 0 17px 50px 0 #F9D689;">
           <img src="${images[0]}" class="card-img-top image" alt="error en imagen">
           <div class="card-body card" style="border: 2px solid #E0A75E; background-color: #FFEEA9;">
             <h5 class="card-title" style="color: #E85C0D">${product.title}</h5>
             <p class="card-text">${product.description}</p>
-            <p class="card-text">${product.value}</p>
-            <Button class="bEditar">Editar</Button>
-            <p></p>
-            <Button onclick="deletePost(${product.id})" class="bEliminar">Eliminar</Button>
+            <p class="card-text">$ ${product.value}</p>
+            <div class="Buttons">
+              <div class="Button1">
+                <Button class="bEditar">
+                  <img src="pen.png" class="iEliminar"/>
+                </Button>
+              </div>
+              <div class="Button1">
+                <Button onclick="deletePost(${product.id})" class="bEliminar">
+                  <img src="delete.png" class="iEliminar"/>
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       `; 
